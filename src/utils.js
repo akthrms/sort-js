045674d25e33964bfed813cd891310e7546cd0d6
akthrms <akt.hrms@gmail.com>
2020-12-05 10:03:37 +0900

    update

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,76 @@
+/**
+ * `arr1`と`arr2`が等しいかどうかを返す
+ *
+ * ```js
+ * equalsArray([1, 2, 3], [1, 2, 3]) == true
+ * ```
+ *
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @returns {boolean}
+ */
 function equalsArray(arr1, arr2) {
   return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
 
+/**
+ * `start`から`end`までの配列を返す（`end`を含む）
+ *
+ * ```js
+ * to(0, 3) == [0, 1, 2, 3]
+ * ```
+ *
+ * @param {number} start
+ * @param {number} end
+ * @returns {Array<number>}
+ */
 function to(start, end) {
   return [...Array(end + 1).keys()].slice(start);
 }
 
+/**
+ * `start`から`end`までの配列を返す（`end`を含まない）
+ *
+ * ```js
+ * until(0, 3) == [0, 1, 2]
+ * ```
+ *
+ * @param {number} start
+ * @param {number} end
+ * @returns {Array<number>}
+ */
 function until(start, end) {
   return to(start, end - 1);
 }
 
+/**
+ * `end`から`start`までの配列を返す
+ *
+ * ```js
+ * downTo(3, 0) == [3, 2, 1, 0]
+ * ```
+ *
+ * @param {number} start
+ * @param {number} end
+ * @returns {Array<number>}
+ */
 function downTo(start, end) {
   return [...to(end, start)].reverse();
 }
 
+/**
+ * `arr[index1]`と`arr[index2]`の要素を入れ替える
+ *
+ * ```js
+ * const arr = [1, 2, 3]
+ * swap(arr, 0, 2)
+ * arr == [3, 2, 1]
+ * ```
+ *
+ * @param {Array} arr
+ * @param {number} index1
+ * @param {number} index2
+ */
 function swap(arr, index1, index2) {
   [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
 }
